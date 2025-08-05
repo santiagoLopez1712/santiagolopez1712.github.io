b3a43750dbdfcd8f0e53c47b649022559701aafc
Santiago López Otalvaro <slopezotalvaro1712@gmail.com>
2025-08-05 11:29:40 +0000

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -348,7 +348,8 @@
                 <svg class="message-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path fill="currentColor" d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H5.2L4 17.2V4h16v12z"/>
                 </svg>
-                Fragen oder Terminwunsch? AMARETIS AI kümmert sich. 
+                Fragen oder einen Termin vereinbaren?<br>  
+                Wir sind jederzeit hier für Sie da! 
             </button>
             <p class="response-text">${config.branding.responseTimeText}</p>
         </div>
