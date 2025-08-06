4807e84780e3694a43a71d1602699a4ffc9f4dd9
Santiago López Otalvaro <slopezotalvaro1712@gmail.com>
2025-08-06 23:25:12 +0200

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -424,15 +424,13 @@
         <div class="new-conversation">
             <h2 class="welcome-text">${config.branding.welcomeText}</h2>
             
-            <button class="new-chat-btn" disabled>
-                <div class="chat-btn-content">
-                    <svg class="message-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H5.2L4 17.2V4h16v12z"/>
-                    </svg><br>
-                </div>
-                Start
-            </button>           
-            <p class="response-text">${config.branding.responseTimeText}</p><br> 
+            <button class="new-chat-btn">
+                <svg class="message-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H5.2L4 17.2V4h16v12z"/>
+                </svg>
+                Schreiben Sie uns :)
+            </button>
+            <p class="response-text">${config.branding.responseTimeText}</p>              
             
             <div class="privacy-checkbox">
                 <input type="checkbox" id="datenschutz" name="datenschutz">
