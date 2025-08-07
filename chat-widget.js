69cdd8b7f026789f28e9e0e11863032f6dd84e71
Santiago López Otalvaro <slopezotalvaro1712@gmail.com>
2025-08-07 11:24:51 +0000

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -150,7 +150,7 @@
 
         .n8n-chat-widget .response-text {
             font-size: 14px;
-            color: #3A6262;
+            color: #000;
             opacity: 0.7;
             margin-bottom:28px;
             font-weight: 400;
@@ -315,9 +315,8 @@
             position: relative;
             padding-left: 28px;
             font-size: 14px;
-            color: #3A6262;
+            color: #000;
             cursor: pointer;
-            line-height: 1.4;
             max-width: 300px;
             font-weight: 400;
             opacity: 0.7;
@@ -452,7 +451,7 @@
                 <svg class="message-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path fill="currentColor" d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H5.2L4 17.2V4h16v12z"/>
                 </svg>
-                Schreiben Sie uns :)
+                Starten Sie Ihre Anfrage!
             </button>             
         </div>
     `;
