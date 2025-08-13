0939fa0ca961e9cac3103fd83c86332f930f0f35
Santiago López Otalvaro <slopezotalvaro1712@gmail.com>
2025-08-13 13:29:00 +0200

    Update chat-widget.js
    Placeholdertext cambiado

--- chat-widget.js
+++ chat-widget.js
@@ -448,7 +448,7 @@
             </div>
             <div class="chat-messages"></div>
             <div class="chat-input">
-                <textarea placeholder="Schreiben Sie uns hier..." rows="1"></textarea>
+                <textarea placeholder="Text oder Sprache eingeben…" rows="1"></textarea>
                 <button type="submit">
                     <!-- Flecha hacia la derecha -->
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
